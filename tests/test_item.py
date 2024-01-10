1592d43ac04d763a453478eaf945744598defad2
elenaludina0573 <elenaludina0@gmail.com>
2024-01-10 23:42:24 +0300

    Checking of class

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+inst = Item("Телефон", 8500, 12)
+def test___init__():
+    assert inst.name == "Телефон"
+    assert inst.price == 8500
+    assert inst.quantity == 12
+
+def test_calculate_total_price():
+    assert inst.calculate_total_price() == 102000
+
+def test_apply_discount():
+    Item.pay_rate = 0.6
+    assert inst.apply_discount() == 5100
+
