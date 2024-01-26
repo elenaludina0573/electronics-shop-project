52b83c5bc74293fe84b5ac4630c89d0ba92dc549
elenaludina0573 <elenaludina0@gmail.com>
2024-01-26 22:43:58 +0300

    Software new class test

---
+++ tests/test_phone.py
@@ -0,0 +1,38 @@
+import pytest
+from src.phone import Phone
+
+
+@pytest.fixture
+def phone():
+    phone = Phone("iPhone 14", 120_000, 5, 2)
+    return phone
+
+
+def test___init__(phone):
+    assert phone.name == "iPhone 14"
+    assert phone.price == 120_000
+    assert phone.quantity == 5
+    assert phone.number_of_sim == 2
+
+
+def test_repr():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
+
+def test_str():
+    phone1 = Phone("iPhone 14", 120000, 5, 2)
+    assert str(phone1) == 'iPhone 14'
+
+
+@pytest.fixture
+def testing_data():
+    testing_data = 1 or 2
+    return testing_data
+
+
+def test_number_of_sim(testing_data):
+    with pytest.raises(ValueError):
+        testing_data.number_of_sim = -1
+        testing_data.number_of_sim = 0
+        testing_data.number_of_sim = 1.2
