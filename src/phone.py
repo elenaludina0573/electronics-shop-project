1490b48eda0f57d02ab50b0d71040a3bcee5b4d8
elenaludina0573 <elenaludina0@gmail.com>
2024-01-26 22:39:30 +0300

    New class is at homework_4

---
+++ src/phone.py
@@ -0,0 +1,35 @@
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        """
+        Создание экземпляра класса Phone.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param number_of_sim: количество поддерживаемых сим-карт
+        """
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, testing_data):
+        if int(testing_data) > 0:
+            self.__number_of_sim = int(testing_data)
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+
