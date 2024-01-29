e7ba502ec2fa431ae1cb92b821e35df14fcfbed6
elenaludina0573 <elenaludina0@gmail.com>
2024-01-30 00:24:10 +0300

    Software new class test homework_5

---
+++ tests/test_keyboard.py
@@ -0,0 +1,27 @@
+import pytest
+from src.keyboard import Keyboard
+
+
+@pytest.fixture
+def kb():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    return kb
+
+
+def test__init__(kb):
+    assert kb.name == 'Dark Project KD87A'
+    assert kb.price == 9600
+    assert kb.quantity == 5
+
+
+def test_language():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    assert str(kb.language) == "EN"
+
+
+def test_change_lang():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+    kb.change_lang()
+    assert str(kb.language) == "EN"
